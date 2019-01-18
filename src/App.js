f9fd02e7d22d9b8a8f2d1bf52c5a192b96b705ef
Samu Joseph <samujoseph@outlook.com>
2019-01-18 16:02:19 +0530

    split line check for merged cells

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const tableData = {
   id: 't1',
   confidenceScore: 50,
   tag: 'Bank details',
-  coordinates: { x: 0, y: 0, width: 200, height: 100 },
+  coordinates: { x: 0, y: 0, width: 380, height: 190 },
   styles: { stroke: 'red', fill: 'transparent' },
   type: 'table',
   tableCols: [
@@ -21,6 +21,18 @@ const tableData = {
     {
       id: 'col2',
       coordinates: { x: 105, y: 5, width: 90, height: 90 },
+      styles: { stroke: 'blue', fill: 'transparent' },
+      type: 'col',
+    },
+    {
+      id: 'col3',
+      coordinates: { x: 205, y: 5, width: 90, height: 90 },
+      styles: { stroke: 'blue', fill: 'transparent' },
+      type: 'col',
+    },
+    {
+      id: 'col4',
+      coordinates: { x: 305, y: 5, width: 90, height: 90 },
       styles: { stroke: 'blue', fill: 'transparent' },
       type: 'col',
     },
@@ -52,6 +64,24 @@ const tableData = {
           coordinates: { x: 100, y: 10, width: 85, height: 35 },
           styles: { stroke: 'yellow', fill: '#ccc' },
           type: 'cell',
+        }, {
+          id: 'cell13',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-11',
+          coordinates: { x: 190, y: 10, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        }, {
+          id: 'cell14',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-12',
+          coordinates: { x: 280, y: 10, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
         },
       ]
     },
@@ -75,10 +105,129 @@ const tableData = {
         {
           id: 'cell22',
           isNull: false,
+          colSpan: 2,
+          rowSpan: 2,
+          value: 'c-22',
+          coordinates: { x: 100, y: 55, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+          merged: true
+        }, {
+          id: 'cell23',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-21',
+          coordinates: { x: 190, y: 55, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+          merged: true,
+        },
+        {
+          id: 'cell24',
+          isNull: false,
           colSpan: 1,
           rowSpan: 1,
           value: 'c-22',
-          coordinates: { x: 100, y: 55, width: 85, height: 35 },
+          coordinates: { x: 280, y: 55, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        },
+      ]
+    },
+    {
+      id: 'row3',
+      isHeader: true,
+      coordinates: { x: 5, y: 105, width: 180, height: 45 },
+      styles: { stroke: 'green', fill: 'transparent' },
+      type: 'row',
+      cells: [
+        {
+          id: 'cell31',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-21',
+          coordinates: { x: 10, y: 100, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        },
+        {
+          id: 'cell32',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-22',
+          coordinates: { x: 100, y: 100, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+          merged: true,
+        }, {
+          id: 'cell33',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-21',
+          coordinates: { x: 190, y: 100, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+          merged: true,
+      },
+        {
+          id: 'cell34',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-22',
+          coordinates: { x: 280, y: 100, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        },
+      ]
+    },
+    {
+      id: 'row4',
+      isHeader: true,
+      coordinates: { x: 5, y: 155, width: 180, height: 45 },
+      styles: { stroke: 'green', fill: 'transparent' },
+      type: 'row',
+      cells: [
+        {
+          id: 'cell41',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-21',
+          coordinates: { x: 10, y: 145, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        },
+        {
+          id: 'cell42',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-22',
+          coordinates: { x: 100, y: 145, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        }, {
+          id: 'cell43',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-21',
+          coordinates: { x: 190, y: 145, width: 85, height: 35 },
+          styles: { stroke: 'yellow', fill: '#ccc' },
+          type: 'cell',
+        },
+        {
+          id: 'cell44',
+          isNull: false,
+          colSpan: 1,
+          rowSpan: 1,
+          value: 'c-22',
+          coordinates: { x: 280, y: 145, width: 85, height: 35 },
           styles: { stroke: 'yellow', fill: '#ccc' },
           type: 'cell',
         },
@@ -95,28 +244,35 @@ const tableCords = [{
   id: tableData.id,
 }];
 
-const tableRowCellCords = tableData.tableRows.reduce((acc, cord) => {
+const tableRowCellCords = tableData.tableRows.reduce((acc, cord, rowIndex) => {
     acc.tableRowCords.push({
     ...cord.coordinates,
     ...cord.styles,
   type: cord.type,
   id: cord.id,
+  rowIndex,
 });
-const cells = cord.cells.map(cell => ({
+const cells = cord.cells.map((cell, columnIndex) => ({
     ...cell.coordinates,
     ...cell.styles,
   type: cell.type,
   id: cell.id,
+  rowIndex,
+  columnIndex,
+  merged: cell.merged,
+  colSpan: cell.colSpan,
+  rowSpan: cell.rowSpan,
 }));
 acc.tableCellCords = [...acc.tableCellCords, ...cells];
 return acc;
 }, { tableRowCords: [], tableCellCords: [] });
 
-const tableColCords = tableData.tableCols.map(col => ({
+const tableColCords = tableData.tableCols.map((col, columnIndex) => ({
     ...col.coordinates,
     ...col.styles,
   type: col.type,
   id: col.id,
+  columnIndex,
 }));
 
 const { tableRowCords, tableCellCords } = tableRowCellCords;
@@ -150,8 +306,46 @@ const drawRectangles = (
 ) => rectangles
   .map((data) => {
     const {
-      x, y, width, height, fill, stroke, className, id,
+      x, y, width, height, fill, stroke, className, id, type, merged, colSpan=1, rowSpan=1,
+      columnIndex, rowIndex
     } = data;
+    if (type === 'cell') {
+      if (merged) {
+        if (colSpan > 1 || rowSpan > 1) {
+          let mergedWidth = 0;
+          let mergedHeight = 0;
+          for (let i = columnIndex; i < columnIndex + colSpan; i++) {
+            mergedWidth += tableData.tableRows[rowIndex].cells[i].coordinates.width;
+          }
+          for (let i = rowIndex; i < rowIndex + rowSpan; i++) {
+            mergedHeight += tableData.tableRows[i].cells[columnIndex].coordinates.height;
+          }
+          const nextData = {
+            ...data,
+            mergedHeight,
+            mergedWidth,
+          };
+          return (
+            <Rectangle
+              id={id}
+              x={x}
+              y={y}
+              width={mergedWidth}
+              height={mergedHeight}
+              stroke={stroke}
+              fill={selectedItems.has(id) ? "blue" : "transparent"}
+              className={classNames(customClassName, className)}
+              onClick={e => {onClick(e, nextData);}}
+              onMouseMove={e => {onMouseMove(e, nextData);}}
+              onMouseOut={e => {onMouseOut(e, nextData);}}
+              selectedItems={selectedItems}
+              fillOpacity={selectedItems.has(id) ? .2 : 1}
+            />
+          );
+        }
+        return null;
+      }
+    }
     return (
       <Rectangle
         id={id}
@@ -211,20 +405,29 @@ class App extends Component {
   }
 
   handleMouseMove(e, item) {
+    console.log('item ', item.id, ' hovered of type: ', item.type, ' for action: ', this.state.editAction, ' data: ', item );
     const getLineCoordinates = (item, hoverCoordinates, axis) => {
-      const getLineCoordinatesByAxis = (item, hoverCoordinates, horizontalAxis) => (
-        horizontalAxis ? ({
-          x1: item.x,
-          y1: hoverCoordinates.y,
-          x2: item.x + item.width,
-          y2: hoverCoordinates.y,
-        }) : ({
-          x1: hoverCoordinates.x,
-          y1: item.y,
-          x2: hoverCoordinates.x,
-          y2: item.y + item.height,
-        })
-      );
+      const getLineCoordinatesByAxis = (item, hoverCoordinates, horizontalAxis) => {
+        let itemWidth = item.width;
+        let itemHeight = item.height;
+        if (item.merged && (item.colSpan > 1 && item.rowSpan >1)) {
+          itemWidth = item.mergedWidth;
+          itemHeight = item.mergedHeight;
+        }
+        return (
+          horizontalAxis ? ({
+            x1: item.x,
+            y1: hoverCoordinates.y,
+            x2: item.x + itemWidth,
+            y2: hoverCoordinates.y,
+          }) : ({
+            x1: hoverCoordinates.x,
+            y1: item.y,
+            x2: hoverCoordinates.x,
+            y2: item.y + itemHeight,
+          })
+        );
+      };
       if (item.type === 'row') {
         return getLineCoordinatesByAxis(item, hoverCoordinates, true)
       } else if (item.type === 'col') {
@@ -411,7 +614,7 @@ class App extends Component {
           </div>
         </header>
         <div className="App-content">
-          <svg ref={r => this.svgRef = r}>
+          <svg ref={r => this.svgRef = r} width={tableData.coordinates.width + 10} height={tableData.coordinates.height + 10} >
             {tableFigure}
             {splitLineCoordinates && drawSplitLine(splitLineCoordinates)}
           </svg>
